0963eed7d8c76e505d01ef49668a3260a0b10c4f
Joao Alfredo <joao.aafn@gmail.com>
2024-11-09 07:53:29 -0300

    feat: add error package

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/joaoaafn/gopportunities.git/router"
+import (
+	"github.com/joaoaafn/gopportunities.git/config"
+	"github.com/joaoaafn/gopportunities.git/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	//Initialize configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
+	//Initialize Router
 	router.Initialize()
 }
